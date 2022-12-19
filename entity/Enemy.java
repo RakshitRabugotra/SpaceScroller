95352991a30634178ae5f53d5628da73dbfb1cd5
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2022-12-19 21:29:17 +0530

    Created Enemy class to create Enemies in the game

---
+++ entity/Enemy.java
@@ -0,0 +1,34 @@
+package SpaceScroller.entity;
+
+import SpaceScroller.constants.Constants;
+import java.awt.Graphics2D;
+
+public class Enemy extends Entity {
+    
+    // Check if the Enemy is active
+    public boolean isActive = false;
+
+    public Enemy(int posX, int posY) {
+        // Call the superclass constructor
+        super(posX, posY, Constants.SCALED_TILESIZE, Constants.SCALED_TILESIZE);
+
+        // Set the active status
+        this.isActive = true;
+    }
+
+    @Override
+    public void update(double dt) {
+        /*
+         * We will move each enemy different way so,
+         * don't do anything here, just clamp the positions
+         */
+        this.x = Math.min(Math.max(this.x, 0), Constants.MAX_SCREEN_COLS - this.width);
+        this.y = Math.min(Math.max(this.y, 0), Constants.MAX_SCREEN_ROWS - this.height);
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        // We don't need to render the Enemy too
+        return;
+    }
+}
