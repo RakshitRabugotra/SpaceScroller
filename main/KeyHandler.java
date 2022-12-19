0c6763fc8ca27318d732d3e8be32c8181580348e
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2022-12-19 20:51:34 +0530

    Added KeyHandler to handle keyboard inputs

---
+++ main/KeyHandler.java
@@ -0,0 +1,50 @@
+package SpaceScroller.main;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    // To assign the key bindings
+    private static int leftKeyBinding = KeyEvent.VK_A;
+    private static int rightKeyBinding = KeyEvent.VK_D;
+    private static int upKeyBinding = KeyEvent.VK_W;
+    private static int downKeyBinding = KeyEvent.VK_S;
+
+    // To keep track of which side we're moving
+    public boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        /*
+         * It'll barely be used
+         */
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        // Fetch the key code and perform necessary actions
+        // As the user has pressed some key
+        int code = e.getKeyCode();
+
+        if(code == leftKeyBinding) leftPressed = true;
+        if(code == rightKeyBinding) rightPressed = true;
+        if(code == upKeyBinding) upPressed = true;
+        if(code == downKeyBinding) downPressed = true;
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // Fetch the key code and perform necessary actions
+        // As the user has released some key
+        int code = e.getKeyCode();
+
+        if(code == leftKeyBinding) leftPressed = false;
+        if(code == rightKeyBinding) rightPressed = false;
+        if(code == upKeyBinding) upPressed = false;
+        if(code == downKeyBinding) downPressed = false;
+        
+    }
+    
+}
