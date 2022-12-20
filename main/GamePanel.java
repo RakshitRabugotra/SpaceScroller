c20df30bd9a2252db3839d4e3595fa2a3399a375
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2022-12-20 08:05:04 +0530

    Removed setFocusable, because the key-inputs were not working

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -7,6 +7,9 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 
 import SpaceScroller.constants.Constants;
+import SpaceScroller.entity.Enemy;
+import SpaceScroller.entity.Player;
+import SpaceScroller.entity.TileMap;
 
 public class GamePanel extends JPanel implements Runnable {
     
@@ -20,12 +23,25 @@ public class GamePanel extends JPanel implements Runnable {
     // Add a key handler to the game
     public KeyHandler keyH = new KeyHandler();
 
+    /*
+     * Declare all the GameObjects here!
+     */
+    public TileMap tileMap = new TileMap(this, keyH);
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Constants.BACKGROUND_COLOR);
         this.setDoubleBuffered(true);
         this.addKeyListener(keyH);
         this.setFocusable(true);
+
+        /*
+         * Set a player to the TileMap
+         */
+        tileMap.setPlayer(Constants.MAX_SCREEN_COLS/2, Constants.MAX_SCREEN_ROWS/2);
+
+        // Add a single enemy to the game
+        tileMap.addEnemy(new Enemy(Constants.MAX_SCREEN_COLS/2, 0));
     }
 
     // Function to initialize the game-thread and to call the run method
@@ -42,6 +58,8 @@ public class GamePanel extends JPanel implements Runnable {
         /*
          * Update all the game entities here!
          */
+        // Update the tilemap
+        tileMap.update(dt);
     }
 
     public void paintComponent(Graphics g) {
@@ -56,7 +74,7 @@ public class GamePanel extends JPanel implements Runnable {
         /*
          * Draw all the game entities here!
          */
-        
+        tileMap.draw(g2);
 
         // Dispose the graphics for better memory management
         g2.dispose();
